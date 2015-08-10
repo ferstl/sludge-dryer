18e3294d76f0a2ef289fdadab8995b148e6ea8d6
Stefan Ferstl <st.ferstl@gmail.com>
2015-08-10 21:22:56 +0200

    Go

---
+++ src/main/java/com/github/ferstl/sludgedrier/Agent.java
@@ -0,0 +1,12 @@
+package com.github.ferstl.sludgedrier;
+
+import java.lang.instrument.Instrumentation;
+
+public class Agent {
+
+  private Agent() {}
+
+  public static void agentmain(String agentArgs, Instrumentation inst) {
+
+  }
+}
